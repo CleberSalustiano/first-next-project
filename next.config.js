d617bbe82f1fdf6df6846266156d9607e2f6eb81
Cleber Salustiano <kleberdejesus45@hotmail.com>
2022-12-12 16:04:02 +0000

    feat: using prisma

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['images.ctfassets.net'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.ctfassets.net',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },
-
 }
 
 module.exports = nextConfig
